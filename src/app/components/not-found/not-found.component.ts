bb445291db189c8935ee057301770209f5885c05
Jayaram Krovvidi <jayaram.krovvidi@outlook.com>
2023-04-25 14:18:12 -0400

    Added Not Found component

---
+++ src/app/components/not-found/not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NotFoundComponent {
+
+}
