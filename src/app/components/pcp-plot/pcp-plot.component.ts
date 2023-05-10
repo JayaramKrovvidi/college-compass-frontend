9e93bb04591dfa269abc66c84bb45045c947f537
Jayaram Krovvidi <jayaram.krovvidi@outlook.com>
2023-05-09 23:25:24 -0400

    Added PCP Plot Initial

--- src/app/components/pcp-plot/pcp-plot.component.ts
+++ src/app/components/pcp-plot/pcp-plot.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import * as d3 from 'd3';
+import ParCoords from 'parcoord-es';
+import { ViewEncapsulation } from '@angular/core';
+import { BackendService } from 'src/app/services/compass-backend-service';
 
 @Component({
   selector: 'pcp-plot',
   templateUrl: './pcp-plot.component.html',
-  styleUrls: ['./pcp-plot.component.scss']
+  styleUrls: ['./pcp-plot.component.scss'],
+  providers: [BackendService],
+  encapsulation: ViewEncapsulation.None
 })
-export class PcpPlotComponent {
+export class PcpPlotComponent implements OnInit {
+
+  backend: BackendService
+  svg; container;
+  constructor(private el: ElementRef, backend: BackendService) {
+    this.backend = backend;
+  }
+
+  ngOnInit(): void {
+    this.x();
+    this.container = this.el.nativeElement.querySelector('#pcp') as HTMLDivElement;
+    this.renderPCP();
+  }
+  renderPCP() {
+    this.removeExistingPlot();
+    const { width, height } = this.getMapContainerWidthAndHeight();
+    this.svg = d3.select(this.container)
+      .attr("style", "width: " + (width + 100) + "px; height: " + height + "px;")
+
+    const colors = this.get_color_generator();
+    console.log(this.svg.node())
+
+    var pcpPlot = ParCoords()(this.svg.node());
+
+    pcpPlot.data(this.backend.getPCPData())
+      .hideAxis(["unit_id"])
+      .color(function (item) { return colors(item["state"]); })
+      .alpha(0.2)
+      .reorderable()
+      .composite("darker")
+      .render()
+      .brushMode("1D-axes");
+  }
+
+  removeExistingPlot() {
+    d3.select(`figure#parcoords`).html("");
+  }
+
+  get_color_generator() {
+    return d3.scaleOrdinal()
+      .domain(["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+        "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+        "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+        "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+        "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"])
+      .range(['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
+      '#17becf', '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
+      '#17becf', '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
+      '#17becf', '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
+      '#17becf', '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']);
+  }
+
+  getMapContainerWidthAndHeight = (): { width: number; height: number } => {
+    const width = this.container.clientWidth;
+    const height = (width / 960) * 650;
+    return { width, height };
+  };
+
+  x() {
+    const stateAbbrs = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+    const colorScale = d3.scaleOrdinal()
+      .domain(stateAbbrs)
+      .range(d3.schemeCategory10);
+    const distinctColors = [];
+
+    for (const state of stateAbbrs) {
+      const color = colorScale(state);
+      distinctColors.push(color);
+    }
+    console.log(distinctColors)
+  }
 
 }
--- src/app/services/compass-backend-service.ts
+++ src/app/services/compass-backend-service.ts
@@ -9,6 +9,19 @@ export interface LocationDataPoint {
     longitude: number;
 }
 
+export interface PCPDataPoint {
+    unit_id: number;
+    avg_applicants_total: number;
+    avg_admissions_total: number;
+    avg_enrolled_total: number;
+    avg_grand_total: number;
+    avg_price_in_st_on_campus: number;
+    avg_price_out_st_on_campus: number;
+    avg_price_in_st_off_campus: number;
+    avg_price_out_st_off_campus: number;
+    state: string;
+  };
+
 export interface MapData {
     colleges: LocationDataPoint[];
 }
@@ -24,4 +37,47 @@ export class BackendService {
     getMapData() {
         return this.http.get<MapData>("http://localhost:6969/map_data");
     }
+
+    getPCPData(): PCPDataPoint[] {
+        return this.generateMockData();
+    }
+
+    generateMockData = (): PCPDataPoint[] => {
+        const states = [
+          "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI",
+          "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI",
+          "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC",
+          "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT",
+          "VT", "VA", "WA", "WV", "WI", "WY"
+        ];
+        
+        const data: PCPDataPoint[] = [];
+      
+        for (let i = 0; i < 2000; i++) {
+          const unit_id = i;
+          const avg_applicants_total = Math.floor(Math.random() * 10000);
+          const avg_admissions_total = Math.floor(Math.random() * 8000);
+          const avg_enrolled_total = Math.floor(Math.random() * 6000);
+          const avg_grand_total = Math.floor(Math.random() * 50000);
+          const avg_price_in_st_on_campus = Math.floor(Math.random() * 20000);
+          const avg_price_out_st_on_campus = Math.floor(Math.random() * 30000);
+          const avg_price_in_st_off_campus = Math.floor(Math.random() * 15000);
+          const avg_price_out_st_off_campus = Math.floor(Math.random() * 25000);
+          const state = states[Math.floor(Math.random() * states.length)];
+          data.push({
+            unit_id,
+            avg_applicants_total,
+            avg_admissions_total,
+            avg_enrolled_total,
+            avg_grand_total,
+            avg_price_in_st_on_campus,
+            avg_price_out_st_on_campus,
+            avg_price_in_st_off_campus,
+            avg_price_out_st_off_campus,
+            state
+          });
+        }
+      
+        return data;
+      };
 }
